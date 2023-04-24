f399eb398c8d62e8df6e509f8c462f0abf5c30f8
Krish Suchak <suchak.krish@gmail.com>
2023-04-24 05:20:30 -0400

    python file

---
+++ gen_task_def.py
@@ -0,0 +1,28 @@
+
+import os
+import json
+
+is_prod = os.environ.get('GITHUB_REF_NAME') == 'master'
+prefix = 'prod' if is_prod else 'dev'
+task_def = {
+    "family": f"{prefix}_eth_staker",
+    "requiresCompatibilities": [
+        "EC2"
+    ],
+    "containerDefinitions": [
+        {
+            "logConfiguration": {
+                "logDriver": "awslogs",
+                "options": {
+                    "awslogs-group": f"{prefix}_eth_staker-container",
+                    "awslogs-region": "us-east-1",
+                    "awslogs-create-group": "true",
+                    "awslogs-stream-prefix": f"{prefix}_eth_staker"
+                }
+            }
+        }
+    ]
+}
+
+with open('task-definition.json', 'w') as file:
+    file.write(json.dumps(task_def, indent=4))
