d06fe51f7aef72b0cedfd91e8d0889c49d2b6646
Krish Suchak <suchak.krish@gmail.com>
2023-04-25 09:42:05 -0400

    commit task-definition

--- gen_task_def.py
+++ gen_task_def.py
@@ -4,6 +4,82 @@ import json
 
 is_prod = os.environ.get('GITHUB_REF_NAME') == 'master'
 prefix = 'prod' if is_prod else 'dev'
+
+task_def = {
+    "taskDefinitionArn": "arn:aws:ecs:us-east-1:092475342352:task-definition/dev_eth_staker:10",
+    "containerDefinitions": [
+        {
+            "name": "dev_staking_container",
+            "image": "092475342352.dkr.ecr.us-east-1.amazonaws.com/dev_eth_staker",
+            "cpu": 0,
+            "portMappings": [
+                {
+                    "containerPort": 80,
+                    "hostPort": 80,
+                    "protocol": "tcp"
+                }
+            ],
+            "essential": true,
+            "entryPoint": [],
+            "command": [],
+            "environment": [],
+            "mountPoints": [],
+            "volumesFrom": [],
+            "logConfiguration": {
+                "logDriver": "awslogs",
+                "options": {
+                    "awslogs-group": "/ecs/dev_eth_staker",
+                    "awslogs-region": "us-east-1",
+                    "awslogs-stream-prefix": "ecs"
+                }
+            }
+        }
+    ],
+    "family": "dev_eth_staker",
+    "executionRoleArn": "arn:aws:iam::092475342352:role/ecsTaskExecutionRole",
+    "networkMode": "awsvpc",
+    "revision": 10,
+    "volumes": [],
+    "status": "ACTIVE",
+    "requiresAttributes": [
+        {
+            "name": "com.amazonaws.ecs.capability.logging-driver.awslogs"
+        },
+        {
+            "name": "ecs.capability.execution-role-awslogs"
+        },
+        {
+            "name": "com.amazonaws.ecs.capability.ecr-auth"
+        },
+        {
+            "name": "com.amazonaws.ecs.capability.docker-remote-api.1.19"
+        },
+        {
+            "name": "ecs.capability.execution-role-ecr-pull"
+        },
+        {
+            "name": "com.amazonaws.ecs.capability.docker-remote-api.1.18"
+        },
+        {
+            "name": "ecs.capability.task-eni"
+        }
+    ],
+    "placementConstraints": [],
+    "compatibilities": [
+        "EC2",
+        "FARGATE"
+    ],
+    "runtimePlatform": {
+        "operatingSystemFamily": "LINUX"
+    },
+    "requiresCompatibilities": [
+        "FARGATE"
+    ],
+    "cpu": "256",
+    "memory": "512",
+    "registeredAt": "2023-04-25T09:38:41.227000-04:00",
+    "registeredBy": "arn:aws:iam::092475342352:root"
+}
 task_def = {
     "executionRoleArn": "arn:aws:iam::092475342352:role/ecsTaskExecutionRole",
     "family": f"{prefix}_eth_staker",
