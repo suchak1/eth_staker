729973e063d06e216f1caf23f00e0c432a6282e3
Krish Suchak <suchak.krish@gmail.com>
2023-05-07 20:27:15 -0700

    goerli relays list

--- stake.py
+++ stake.py
@@ -24,11 +24,26 @@ max_snapshots = 3
 snapshot_days = 30
 max_snapshot_days = max_snapshots * snapshot_days
 
+goerli_relays = [
+    # Flashbots
+    'https://0xafa4c6985aa049fb79dd37010438cfebeb0f2bd42b115b89dd678dab0670c1de38da0c4e9138c9290a398ecd9a0b3110@builder-relay-goerli.flashbots.net',
+    # bloXroute max profit
+    'https://0x821f2a65afb70e7f2e820a925a9b4c80a159620582c1766b1b09729fec178b11ea22abb3a51f07b288be815a1a2ff516@bloxroute.max-profit.builder.goerli.blxrbdn.com',
+    # Blocknative
+    'https://0x8f7b17a74569b7a57e9bdafd2e159380759f5dc3ccbd4bf600414147e8c4e1dc6ebada83c0139ac15850eb6c975e82d0@builder-relay-goerli.blocknative.com',
+    # Eden
+    'https://0xb1d229d9c21298a87846c7022ebeef277dfc321fe674fa45312e20b5b6c400bfde9383f801848d7837ed5fc449083a12@relay-goerli.edennetwork.io',
+    # Manifold
+    'https://0x8a72a5ec3e2909fff931c8b42c9e0e6c6e660ac48a98016777fc63a73316b3ffb5c622495106277f8dbcc17a06e92ca3@goerli-relay.securerpc.com',
+    # Aestus
+    'https://0xab78bf8c781c58078c3beb5710c57940874dd96aef2835e7742c866b4c7c0406754376c2c8285a36c630346aa5c5f833@goerli.aestus.live',
+    # Ultra Sound
+    'https://0xb1559beef7b5ba3127485bbbb090362d9f497ba64e177ee2c8e7db74746306efad687f2cf8574e38d70067d40ef136dc@relay-stag.ultrasound.money'
+]
 
-def term_node_after_int(node):
-    sleep(5)
-    node.terminate()
-    sleep(5)
+mainnet_relays = [
+
+]
 
 
 class Snapshot:
@@ -262,13 +277,14 @@ class Node:
     def run(self):
 
         while True:
+            # INSERT MEV REPLAY SHUFFLE LOGIC HERE
+            # create a list of good ones from global relays list and save to self.fast_relays
+            # use ','.join(fast_relays) ? look up cli arg format
             self.start()
             backup_is_recent = True
             sent_interrupt = False
             # start = time()
             # since_signal = time()
-            # INSERT MEV REPLAY SHUFFLE LOGIC HERE
-            # save good replay urls to self?
             try:
                 while True:
                     if self.snapshot.is_older_than(self.most_recent, 30):
@@ -282,7 +298,9 @@ class Node:
                         self.print_line(meta['prefix'], meta['stdout'])
             except Exception as e:
                 logging.exception(e)
-            term_node_after_int(self)
+            sleep(5)
+            self.terminate()
+            sleep(5)
             self.kill()
             self.most_recent = self.snapshot.backup()
 
@@ -292,7 +310,9 @@ node = Node()
 
 def stop_node(*_):
     node.interrupt()
-    term_node_after_int(node)
+    sleep(3)
+    node.terminate()
+    sleep(3)
     print('Node stopped.')
     exit(0)
 
