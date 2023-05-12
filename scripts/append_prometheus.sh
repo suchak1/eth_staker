23a5bb4eaddfe416408ef87e5f828fb93dce6d38
Krish Suchak <suchak.krish@gmail.com>
2023-05-11 17:04:37 -0700

    use -e flag for echo

--- scripts/append_prometheus.sh
+++ scripts/append_prometheus.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -eu
+
 if [[ $CI == true ]]; then
 
   base_yml=$(cat extra/prometheus_base.yml)
@@ -6,7 +10,8 @@ if [[ $CI == true ]]; then
   - url: https://prometheus-prod-13-prod-us-east-0.grafana.net/api/prom/push
     basic_auth:
       username: ${GRAF_USER}
-      password: ${GRAF_PASS}"
-
-  echo "${base_yml}\n${append_write}" > extra/prometheus.yml
+      password: ${GRAF_PASS}
+  "
+  # Use -e flag to evaluate backslashes
+  echo -e "${base_yml}\n${append_write}" > extra/prometheus.yml
 fi
