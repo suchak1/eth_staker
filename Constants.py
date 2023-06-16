228fe156371b096bf0d6d22ba0411e99bedd9d60
Krish Suchak <suchak.krish@gmail.com>
2023-06-16 05:36:50 -0400

    reduce max_peers

--- Constants.py
+++ Constants.py
@@ -6,7 +6,7 @@ BEACONCHAIN_KEY = os.environ['BEACONCHAIN_KEY']
 ETH_ADDR = os.environ['ETH_ADDR']
 DEV = DEPLOY_ENV.lower() == 'dev'
 KILL_TIME = 15
-MAX_PEERS = 25
+MAX_PEERS = 10
 
 
 def get_env_bool(var_name):
