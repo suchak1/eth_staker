7b6c0242f3091d5ad9f34d9187ee96a47f0ffba5
Krish Suchak <suchak.krish@gmail.com>
2023-07-03 23:13:58 -0400

    run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 set -eu
+source config.env
+
+docker run \
+    --env ETH_ADDR="${ETH_ADDR}" \
+    --env=BEACONCHAIN_KEY="" \
+    --env AWS_DEFAULT_REGION=us-east-1 \
+    --env DOCKER=true \
+    --cap-add=NET_ADMIN \
+    --device=/dev/net/tun \ 
+    -p 30303:30303/tcp \
+    -p 30303:30303/udp \
+    -p 13000:13000/tcp \
+    -p 12000:12000/udp \
+    -v ~:/mnt/ebs \
+    ethereum
