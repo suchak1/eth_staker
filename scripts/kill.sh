70056dd15f29ca836cede5aabe2b508629de1ea4
Krish Suchak <suchak.krish@gmail.com>
2023-09-18 03:01:12 -0400

    improve kill script

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -4,3 +4,10 @@ set -eu
 source config.env
 
 docker kill --signal=SIGINT ethereum
+
+CMD=$(docker ps | grep ethereum)
+while [[ $? -eq 0 ]]
+do 
+    sleep 1
+    CMD=$(docker ps | grep ethereum)
+done
