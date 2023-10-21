84e0130dec0e48e231ca0ca11da5ce135b639090
Krish Suchak <suchak.krish@gmail.com>
2023-10-21 03:38:09 -0400

    make restore script execute fr

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -4,4 +4,4 @@ set -eu
 
 sudo mkdir -p /mnt/e
 sudo mount -t drvfs E: /mnt/e
-sudo rsync -avnh --info=progress2 /mnt/e/.ethereum /mnt/e/.eth2 ~
+sudo rsync -avh --info=progress2 /mnt/e/.ethereum /mnt/e/.eth2 ~
