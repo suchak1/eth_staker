c49cd20d01a30ac735d3819c8ccdba04b9e59314
Krish Suchak <suchak.krish@gmail.com>
2024-03-31 15:37:33 -0400

    change backup drive

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-sudo mkdir -p /mnt/e
-sudo mount -t drvfs E: /mnt/e
-sudo rsync -avh --info=progress2 ~/.ethereum ~/.eth2 /mnt/e
+sudo mkdir -p /mnt/d
+sudo mount -t drvfs D: /mnt/d
+sudo rsync -avh --info=progress2 ~/.ethereum ~/.eth2 /mnt/d
--- scripts/restore.sh
+++ scripts/restore.sh
@@ -2,6 +2,6 @@
 
 set -eu
 
-sudo mkdir -p /mnt/e
-sudo mount -t drvfs E: /mnt/e
-sudo rsync -avh --info=progress2 /mnt/e/.ethereum /mnt/e/.eth2 ~
+sudo mkdir -p /mnt/d
+sudo mount -t drvfs D: /mnt/d
+sudo rsync -avh --info=progress2 /mnt/d/.ethereum /mnt/d/.eth2 ~
