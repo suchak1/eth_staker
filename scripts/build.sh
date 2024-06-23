f55eaa768bf9f9a36e69ffcee69c6f7fde527dbc
Krish Suchak <suchak.krish@gmail.com>
2024-06-23 15:04:49 -0400

    change build cmd

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,11 @@
 
 set -eu
 
-docker build -t ethereum --build-arg DEPLOY_ENV=prod --build-arg ARCH=amd64 --build-arg VPN=true .
+docker build \
+    -t ethereum \
+    --network=host \
+    --build-arg DEPLOY_ENV=prod \
+    --build-arg ARCH=amd64 \
+    --build-arg \
+    VPN=true \
+    .
